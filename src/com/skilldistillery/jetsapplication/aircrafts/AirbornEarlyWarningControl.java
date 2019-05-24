5c17fc93b74565bc551875df60b8778f1aeb9b0f
Ryan Harrington <harrington.ryana@gmail.com>
2019-05-24 15:08:51 -0600

    Create EAWC class

---
+++ src/com/skilldistillery/jetsapplication/aircrafts/AirbornEarlyWarningControl.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.jetsapplication.aircrafts;
+
+public class AirbornEarlyWarningControl extends Aircraft{
+
+	public AirbornEarlyWarningControl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AirbornEarlyWarningControl(String category, String model, int inService, int speed, int range, int price) {
+		super(category, model, inService, speed, range, price);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
