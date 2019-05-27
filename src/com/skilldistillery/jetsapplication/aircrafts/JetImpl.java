e1143b1449e01b870ea63e0d344c42fb61b5c894
Ryan Harrington <harrington.ryana@gmail.com>
2019-05-27 15:34:07 -0600

    Create new jet using default JetImpl

---
+++ src/com/skilldistillery/jetsapplication/aircrafts/JetImpl.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.jetsapplication.aircrafts;
+
+public class JetImpl extends Aircraft {
+
+	public JetImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public JetImpl(String category, String model, int inService, int speed, int range, int price) {
+		super(category, model, inService, speed, range, price);
+		// TODO Auto-generated constructor stub
+	}
+
+}
